b94b259f003164b88124c9c9d293addc042b2b56
deki08 <teamsoul234@gmail.com>
2024-04-08 00:03:28 +0530

    added config

--- src/main/java/com/vidring/VidringBillingApplication.java
+++ src/main/java/com/vidring/VidringBillingApplication.java
@@ -2,18 +2,17 @@ package com.vidring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-public class VidringBillingApplication extends SpringBootServletInitializer {
+@EntityScan(basePackageClasses = { VidringBillingApplication.class, Jsr310JpaConverters.class })
+@ComponentScan("com.vidring")
+public class VidringBillingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VidringBillingApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(VidringBillingApplication.class);
-	}
 }
