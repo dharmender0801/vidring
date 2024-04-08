7e404c7e2247a90ded682a17dc8b327840d962ca
dharmender0801 <teamsoul234@gmail.com>
2024-04-08 14:57:52 +0530

    removed swagger

--- src/main/java/com/vidring/config/SwaggerConfig.java
+++ src/main/java/com/vidring/config/SwaggerConfig.java
@@ -9,21 +9,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
-@Component
+//@Component
 public class SwaggerConfig {
 
-	@Bean
-	public GroupedOpenApi publicApi() {
-		return GroupedOpenApi.builder().group("springshop-public").pathsToMatch("/**").build();
-	}
-
-	@Bean
-	public OpenAPI springShopOpenAPI() {
-		return new OpenAPI()
-				.info(new Info().title("Vidring Billing API").description("Billing API'S").version("v0.0.1")
-						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
-				.externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation")
-						.url("https://springshop.wiki.github.org/docs"));
-	}
+//	@Bean
+//	public GroupedOpenApi publicApi() {
+//		return GroupedOpenApi.builder().group("springshop-public").pathsToMatch("/**").build();
+//	}
+//
+//	@Bean
+//	public OpenAPI springShopOpenAPI() {
+//		return new OpenAPI()
+//				.info(new Info().title("Vidring Billing API").description("Billing API'S").version("v0.0.1")
+//						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+//				.externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation")
+//						.url("https://springshop.wiki.github.org/docs"));
+//	}
 
 }
