743c75d6af0aeec9c38bfcbfc75d92936f0108c2
dharmender0801 <teamsoul234@gmail.com>
2024-04-08 15:29:06 +0530

    adedd javax dependency

--- src/main/java/com/vidring/model/VidringUnSubscriptionModel.java
+++ src/main/java/com/vidring/model/VidringUnSubscriptionModel.java
@@ -2,15 +2,12 @@ package com.vidring.model;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 import lombok.ToString;
 
