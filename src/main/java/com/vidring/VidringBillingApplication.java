48f1656193710e3d04c881ed58637dfcf4bb1fe2
dharmender0801 <teamsoul234@gmail.com>
2024-04-08 14:34:42 +0530

    added new changes for java version

--- src/main/java/com/vidring/VidringBillingApplication.java
+++ src/main/java/com/vidring/VidringBillingApplication.java
@@ -2,13 +2,8 @@ package com.vidring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = { VidringBillingApplication.class, Jsr310JpaConverters.class })
-@ComponentScan("com.vidring")
 public class VidringBillingApplication {
 
 	public static void main(String[] args) {
